536443c7c7ff87907c6cd28360e9860393e472f7
freddy <perezescalantefreddy@gmail.com>
2024-05-21 23:39:37 -0500

    dd

--- prueba.go
+++ prueba.go
@@ -1,3 +1,14 @@
-func TestNombre(t *testing.T) {
-    // código de prueba
+
+package paquetes
+
+import (
+    "testing"
+)
+
+func TestSuma(t *testing.T) {
+    resultado := Suma(2, 3)
+    esperado := 5
+    if resultado != esperado {
+        t.Errorf("Suma(2, 3) = %d; se esperaba %d", resultado, esperado)
+    }
 }
